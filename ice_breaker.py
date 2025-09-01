55be857739cb4ccdc4878089e3bcc238924b1d78
VaroonSB <varoonsb1311@gmail.com>
2025-09-01 21:22:38 +0530

    twitter scraping and summarization with tool calling - agentic

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,27 +4,34 @@ from langchain_groq import ChatGroq
 
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
+from third_parties.twitter import scrape_user_tweets
 
 
 def ice_break_with(name: str):
     linkedin_username = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
 
-    summary_template = """
-    given the Linkedin information {information} about a person I want you to create:
-    1. A short summary
-    2. two interesting facts about them
-    """
+    twitter_username = twitter_lookup_agent(name=name)
+    tweets = scrape_user_tweets(username=twitter_username, mock=True)
 
+    summary_template = """
+    given the information about a person from linkedin {information},
+    and their latest twitter posts {twitter_posts} I want you to create:
+    1. A short summary
+    2. two interesting facts about them 
+
+    Use both information from twitter and Linkedin
+    """
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information", "twitter_posts"], template=summary_template
     )
 
     llm = ChatGroq(model="openai/gpt-oss-20b", temperature=0)
 
     chain = summary_prompt_template | llm
 
-    res = chain.invoke(input={"information": linkedin_data})
+    res = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
 
     print(res)
 
